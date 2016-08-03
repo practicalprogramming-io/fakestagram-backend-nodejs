9d79b01dd4e93b02fbd8fdb57d10d80a25166511
Adrian Sonnenschein <adrian.sonnenschein@gmail.com>
2016-08-02 20:58:09 -0700

    figured out nested routes

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -4,13 +4,15 @@ import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import Landing from './containers/Landing'
 import Home from './containers/Home'
+import App from './containers/App'
 
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path="/" component={Landing}>
+    <Route path="/" component={Landing} />
+    <Route path="/" component={App}>
+      <Route name="home" path="/:username/" component={Home} />
     </Route>
-    <Route name="home" path="/:username/" component={Home} />
   </Router>,
   document.getElementById('fakestagram')
 )
